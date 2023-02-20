158eacc7ce5fae2823fe4b7455a451b391f1ec6e
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-20 15:25:46 -0800

    edited photo, formatted navb, added participants

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import logo from '../assets/3030-logo.svg'
 
 function NavBar() {
   return (
-    <div className='flex h-[10vh]'>
-      <div className='flex bg-[#0d8360] w-[50%]  text-white h-[10vh]'>
+    <div className='flex'>
+      <div className='flex bg-[#0d8360] w-[50%]  text-white '>
         <img className='w-[25%] m-2' src={logo} alt="3030 logo" />
         <p className='md:text-xs lg:text-lg md:m-3 lg:m-4 leading-relaxed font-harmonicaCondensed'>
           The 30 30 Campaign was created in 2013 to celebrate AIDS Vancouver's
@@ -12,7 +12,7 @@ function NavBar() {
             HIV in Vancouver, told by and for those who were there.</p>
       </div>
       <div className='bg-[#0b6c4f] w-[50%] '>
-        <ul className=' h-[10vh] flex justify-end items-center font-harmonicaReg'>
+        <ul className=' flex justify-end items-center font-harmonicaReg h-full'>
           <li className='mx-8 text-[#d5d5d5] underline'>Stories</li>
           <li className='mx-8 font-semibold text-white underline'>Acknowledgements</li>
         </ul>
--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -2,8 +2,80 @@ import React from 'react'
 
 function Participants() {
   return (
-    <div>
-      
+    <div className='bg-[#0d8360]'>
+      <h1 className='font-harmonicaBold text-white text-7xl pt-[10rem] pb-[3rem] pl-[4rem]'>
+        Participants
+        </h1>
+   <div className='flex mx-[4rem] pb-[6rem]'>
+        <div className='w-1/4'>
+          <ul className='text-white text-3xl font-harmonicaReg'>
+            <li className='my-4'>Alan Herbert</li>
+            <li className='my-4'>Bradford McIntyre</li>
+            <li className='my-4'>Ann Beaufoy</li>
+            <li className='my-4'>Brian Chittok</li>
+            <li className='my-4'>Dr. Brian Willoughby</li>
+            <li className='my-4'>Bronwyn Barrett</li>
+            <li className='my-4'>Caitlin Padgett</li>
+            <li className='my-4'>Carl Bognar</li>
+            <li className='my-4'>Chris Buchner</li>
+            <li className='my-4'>Daphne Spencer</li>
+            <li className='my-4'>Dr. Mark Gilbert</li>
+            <li className='my-4'>Dr. Mike Rekart</li>
+          </ul>
+        </div>
+        <div className='w-1/4'>
+          <ul className='text-white text-3xl font-harmonicaReg'>
+            <li className='my-4'>Dr. O'Shaughnessy</li>
+            <li className='my-4'>Easter Armas</li>
+            <li className='my-4'>Ed Lee</li>
+            <li className='my-4'>Gillian Maxwell</li>
+            <li className='my-4'>Gordon Price</li>
+            <li className='my-4'>Grant 'Bubbles'</li>
+            <li className='my-4'>Hon. Dr. Hedy Fry</li>
+            <li className='my-4'>Heidi Morgan</li>
+            <li className='my-4'>Henry Koo</li>
+            <li className='my-4'>Irene Goldstone</li>
+            <li className='my-4'>Jacquie Haywood</li>
+            <li className='my-4'>James Johnstone</li>
+          </ul>
+        </div>
+        <div className='w-1/4'>
+          <ul className='text-white text-3xl font-harmonicaReg'>
+            <li className='my-4'>Janet Madsen</li>
+            <li className='my-4'>Dr. Jay Wortman</li>
+            <li className='my-4'>John Bishop</li>
+            <li className='my-4'>Dr. John Blatherwick</li>
+            <li className='my-4'>John Carswell</li>
+            <li className='my-4'>Judy Weiser</li>
+            <li className='my-4'>Dr. Julio Montaner</li>
+            <li className='my-4'>Ken Clements</li>
+            <li className='my-4'>Marcie Summers</li>
+            <li className='my-4'>Mark Mees</li>
+            <li className='my-4'>Mary Jackson</li>
+            <li className='my-4'>Maxine Davis</li>
+          </ul>
+        </div>
+        <div className='w-1/4'>
+          <ul className='text-white text-3xl font-harmonicaReg'>
+            <li className='my-4'>May McQueen</li>
+            <li className='my-4'>Mayor Gregor Robertson</li>
+            <li className='my-4'>Michael Vonn</li>
+            <li className='my-4'>Michael Welsh</li>
+            <li className='my-4'>Miranda Compton</li>
+            <li className='my-4'>Moffatt Clarke</li>
+            <li className='my-4'>Noah Stewartt</li>
+            <li className='my-4'>Pat Carney</li>
+            <li className='my-4'>Phillip Banks</li>
+            <li className='my-4'>Richard Rooney</li>
+            <li className='my-4'>Rick Marchand</li>
+            <li className='my-4'>Valerie Nicholson</li>
+          </ul>
+        </div>
+
+   </div>
+  
+    
+    
     </div>
   )
 }
