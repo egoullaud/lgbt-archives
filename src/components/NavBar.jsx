64e5f72b41b9f3f969d0846ebbd9b232f30339e0
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-20 17:03:11 -0800

    edited text size

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     <div className='flex'>
       <div className='flex bg-[#0d8360] w-[50%]  text-white '>
         <img className='w-[25%] m-2' src={logo} alt="3030 logo" />
-        <p className='md:text-xs lg:text-lg md:m-3 lg:m-4 leading-relaxed font-harmonicaCondensed'>
+        <p className='md:text-xs lg:text-lg p-6 mr-2 leading-relaxed font-harmonicaReg'>
           The 30 30 Campaign was created in 2013 to celebrate AIDS Vancouver's
            30th anniversary. The campaign pieces together the dynamic history of
             HIV in Vancouver, told by and for those who were there.</p>
